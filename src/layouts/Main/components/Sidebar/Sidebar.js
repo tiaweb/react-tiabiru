ad0a02eb2a36bb2e90f5a01e95b91f021279c35c
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-04-27 18:42:25 +0700

    Update Sidebar.js

--- src/layouts/Main/components/Sidebar/Sidebar.js
+++ src/layouts/Main/components/Sidebar/Sidebar.js
@@ -85,7 +85,28 @@ const Sidebar = props => {
     }
   ];
 
-  
+  return (
+    <Drawer
+      anchor="left"
+      classes={{ paper: classes.drawer }}
+      onClose={onClose}
+      open={open}
+      variant={variant}
+    >
+      <div
+        {...rest}
+        className={clsx(classes.root, className)}
+      >
+        <Profile />
+        <Divider className={classes.divider} />
+        <SidebarNav
+          className={classes.nav}
+          pages={pages}
+        />
+        <UpgradePlan />
+      </div>
+    </Drawer>
+  );
 };
 
 Sidebar.propTypes = {
