0483e6b527fe48366686324f8645520d6f424868
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-04-27 18:24:44 +0700

    Update Profile.js

--- src/layouts/Main/components/Sidebar/components/Profile/Profile.js
+++ src/layouts/Main/components/Sidebar/components/Profile/Profile.js
@@ -27,9 +27,9 @@ const Profile = props => {
   const classes = useStyles();
 
   const user = {
-    name: 'Shen Zhi',
+    name: 'Tiaweb',
     avatar: '/images/avatars/avatar_11.png',
-    bio: 'Brain Director'
+    bio: 'Hello word'
   };
 
   return (
