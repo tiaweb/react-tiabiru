04dd10c40ca1c5152a8ff1edf96d109d93da4c7b
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-04-29 10:05:00 +0700

    Update Topbar.js

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import Search from './components/Search';
+import Search from './components';
 
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
